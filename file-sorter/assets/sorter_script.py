3a6c3db3ac3b712f3944fe5e7e237a83a8681130
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-10-30 16:44:32 +0000

    Changed all instances of os.rename to shutil.move. This also allowed the removal of the try except blocks if the file already existed at the destination

--- file-sorter/assets/sorter_script.py
+++ file-sorter/assets/sorter_script.py
@@ -1,35 +1,55 @@
 import os
+import shutil
 import time
 import tkinter as tk
 from datetime import datetime
-from tkinter import ttk, filedialog
+from tkinter import filedialog, ttk
 
-############################################################
-###SORTER###################################################
-############################################################
+# SORTER ---------------------------------------------------------------------------------------
+
+
 class Sorter(object):
-    """Contains functions for making sort folders and sorting files into those folders. Class variables are used by these functions."""
+    """Contains functions for generating sort folders and sorting files into those folders."""
+
     def __init__(self):
         # DEFAULTS AND CONSTANTS
         self.sort_type = "file type"
         # folder to be sorted
         self.sorted_folder = "./"
         # folders files will be sorted into
-        self.folders = ["Set-up or Program Files", "Compressed Archives", "Images", "Text Files", "Other"]
+        self.folders = [
+            "Set-up or Program Files",
+            "Compressed Archives",
+            "Images",
+            "Text Files",
+            "Other",
+        ]
         # Current date
         self.today = datetime.today()
         # current year, and earliest year for sort (can be changed)
         self.YEAR = self.today.year
         self.earliest = self.today.year
-        # folder names in each year folder
-        self.MONTHS = ["(1) Jan", "(2) Feb", "(3) Mar", "(4) Apr", "(5) May", "(6) Jun", "(7) Jul", "(8) Aug", "(9) Sep", "(10) Oct", "(11) Nov", "(12) Dec"]
+        # folder names to be contained in each year folder
+        self.MONTHS = [
+            "(1) Jan",
+            "(2) Feb",
+            "(3) Mar",
+            "(4) Apr",
+            "(5) May",
+            "(6) Jun",
+            "(7) Jul",
+            "(8) Aug",
+            "(9) Sep",
+            "(10) Oct",
+            "(11) Nov",
+            "(12) Dec",
+        ]
 
-    #METHODS
+    # METHODS
     def select_folder(self):
         """Pop up to ask user to select a directory to be sorted"""
 
         self.sorted_folder = filedialog.askdirectory() + "/"
-
 
     def date_year_selection(self):
         """
@@ -49,28 +69,38 @@ class Sorter(object):
                 date_pop_up.destroy()
                 self.wait_for_me = False
 
-
             except:
                 bad_entry = tk.Toplevel()
                 bad_entry.wm_title("Error in year selection")
-                bad_entry_label = tk.Label(bad_entry, text="Please enter a valid year.\nMust be less than or equal to current year.")
+                bad_entry_label = tk.Label(
+                    bad_entry,
+                    text="Please enter a valid year.\nMust be less than or equal to current year.",
+                )
                 bad_entry_label.pack()
-                bad_entry_button = ttk.Button(bad_entry, text="Ok", command=bad_entry.destroy)
+                bad_entry_button = ttk.Button(
+                    bad_entry, text="Ok", command=bad_entry.destroy
+                )
                 bad_entry_button.pack()
-
 
         date_pop_up = tk.Toplevel()
         date_pop_up.wm_title("Year Selection")
 
-        year_entry_label = tk.Label(date_pop_up, font=("Helvetica", 10), text="Please enter the earliest year you want\nto create sorted folders for.\nFolders will be created from that year to the current year.\nPlease note that low numbers will create an\nexcessive amount of folders.")
+        year_entry_label = tk.Label(
+            date_pop_up,
+            font=("Helvetica", 10),
+            text="Please enter the earliest year you want\nto create sorted folders for.\nFolders will be created from that year to the current year.\nPlease note that low numbers will create an\nexcessive amount of folders.",
+        )
         year_entry_label.pack()
 
         year_entry = tk.Entry(date_pop_up)
         year_entry.pack()
 
-        year_entry_button = ttk.Button(date_pop_up, text="Submit", command=lambda: validate_and_set_year(year_entry.get()))
+        year_entry_button = ttk.Button(
+            date_pop_up,
+            text="Submit",
+            command=lambda: validate_and_set_year(year_entry.get()),
+        )
         year_entry_button.pack()
-
 
     def select_sort_type(self):
         """
@@ -86,21 +116,26 @@ class Sorter(object):
             self.date_year_selection()
             self.sort_type_pop_up.destroy()
 
-
         def set_to_type_and_destroy(self):
             self.sort_type = "file type"
             self.sort_type_pop_up.destroy()
             self.wait_for_me = False
 
-
         self.sort_type_pop_up = tk.Toplevel()
         self.sort_type_pop_up.wm_title("Sort Type")
-        #pop_up_buttons
-        date_sort = ttk.Button(self.sort_type_pop_up, text="Sort by date", command= lambda: set_to_date_and_destroy(self))
+        # pop_up_buttons
+        date_sort = ttk.Button(
+            self.sort_type_pop_up,
+            text="Sort by date",
+            command=lambda: set_to_date_and_destroy(self),
+        )
         date_sort.grid(row=0, column=0)
-        file_type_sort = ttk.Button(self.sort_type_pop_up, text="Sort by file type", command= lambda: set_to_type_and_destroy(self))
+        file_type_sort = ttk.Button(
+            self.sort_type_pop_up,
+            text="Sort by file type",
+            command=lambda: set_to_type_and_destroy(self),
+        )
         file_type_sort.grid(row=0, column=1)
-
 
     def check_folders(self, folders):
         """Ensures folders exist in sorted_folder directory, creates them if not found"""
@@ -108,13 +143,14 @@ class Sorter(object):
         for folder in folders:
             path = "".join([self.sorted_folder, str(folder)])
             if not os.path.exists(path):
-                 os.mkdir(path)
-
+                os.mkdir(path)
 
     def make_date_folders(self):
         """Checks that folders for years and folders for months in each year exist and sets self.folders to years between earliest and current year"""
 
-        self.folders = [str(year) for year in range(int(self.earliest), int(self.YEAR) + 1)]
+        self.folders = [
+            str(year) for year in range(int(self.earliest), int(self.YEAR) + 1)
+        ]
 
         # makes year folders
         self.check_folders(self.folders)
@@ -123,23 +159,41 @@ class Sorter(object):
             for month in self.MONTHS:
                 y_path = "".join([self.sorted_folder, year, "/", month])
                 if not os.path.exists(y_path):
-                     os.mkdir(y_path)
-
+                    os.mkdir(y_path)
 
     def make_file_folders(self):
         """Sets folders to file types and makes the folders"""
 
-        self.folders = ["Set-up or Program Files", "Compressed Archives", "Images", "Text Files", "Other"]
+        self.folders = [
+            "Set-up or Program Files",
+            "Compressed Archives",
+            "Images",
+            "Text Files",
+            "Other",
+        ]
         self.check_folders(self.folders)
-
 
     def sort_to_folder_by_date(self):
         """Sorts files into respective year/month folders by looking at the last modification date of each file, if sort type == date"""
 
         # Used so folder names for each month are '(1) Jan', '(2) Feb' etc.
-        months = {"Jan": "(1)", "Feb": "(2)", "Mar": "(3)", "Apr": "(4)", "May": "(5)", "Jun": "(6)", "Jul": "(7)", "Aug": "(8)", "Sep": "(9)", "Oct": "(10)", "Nov": "(11)", "Dec": "(12)"}
+        months = {
+            "Jan": "(1)",
+            "Feb": "(2)",
+            "Mar": "(3)",
+            "Apr": "(4)",
+            "May": "(5)",
+            "Jun": "(6)",
+            "Jul": "(7)",
+            "Aug": "(8)",
+            "Sep": "(9)",
+            "Oct": "(10)",
+            "Nov": "(11)",
+            "Dec": "(12)",
+        }
 
         for filename in os.listdir(self.sorted_folder):
+            # So as to not move any of the auto generated folders
             if not filename in self.folders:
                 old_path = "".join([self.sorted_folder, filename])
                 # finding date of last modification for each file
@@ -149,22 +203,21 @@ class Sorter(object):
 
                 # If a year folder for the file does not exist, skip this file
                 if int(lst[4]) < int(self.earliest):
-                    print(f"{filename} was last modified in {str(lst[4])} and a folder for that year does not exist, so the file was skipped during the sort")
+                    print(
+                        f"{filename} was last modified in {str(lst[4])} and a folder for that year does not exist, so the file was skipped during the sort"
+                    )
                     continue
-                
+
                 month = str(lst[1])
-                new_path = f'{self.sorted_folder}{str(lst[4])}/{months[month]} {month}/{filename}' 
-                # try block so program doesn't stop if file already exists in directory. Exising file is replaced by file being moved
-                try:
-                    os.rename(old_path, new_path)
-                except FileExistsError:
-                    os.remove(new_path)
-                    os.rename(old_path, new_path)
+                new_path = f"{self.sorted_folder}{str(lst[4])}/{months[month]} {month}/{filename}"
 
+                shutil.move(old_path, new_path)
 
-    # insert elif statement above else statement if adding a folder to the list folders
     def assign_folder_by_file_type(self, filename, folders):
         """returns folder name based on file name extension i.e. .exe"""
+
+        # Insert elif statement above else statement if you added
+        # a folder to the list self.folders
 
         if filename.lower().endswith((".exe", ".app", ".dmg")):
             return folders[0]
@@ -172,11 +225,12 @@ class Sorter(object):
             return folders[1]
         elif filename.lower().endswith((".ico", ".jpg", ".jpeg", ".png", ".pdf")):
             return folders[2]
-        elif filename.lower().endswith((".doc", ".docx", ".docm", ".rtx", ".txt", ".ini")):
+        elif filename.lower().endswith(
+            (".doc", ".docx", ".docm", ".rtx", ".txt", ".ini")
+        ):
             return folders[3]
         else:
             return folders[-1]
-
 
     def sort_to_folder_by_file_type(self):
         """Sort function if sort type == file type"""
@@ -184,13 +238,16 @@ class Sorter(object):
         for filename in os.listdir(self.sorted_folder):
             if not filename in self.folders:
                 old_path = "".join([self.sorted_folder, filename])
-                new_path = "".join([self.sorted_folder, self.assign_folder_by_file_type(filename, self.folders), "/", filename])
-                try:
-                    os.rename(old_path, new_path)
-                except FileExistsError:
-                    os.remove(new_path)
-                    os.rename(old_path, new_path)
+                new_path = "".join(
+                    [
+                        self.sorted_folder,
+                        self.assign_folder_by_file_type(filename, self.folders),
+                        "/",
+                        filename,
+                    ]
+                )
 
+                shutil.move(old_path, new_path)
 
     def sort_files(self, sort_type=None):
         """Final function for sorting files, run this to sort."""
@@ -207,16 +264,28 @@ class Sorter(object):
             self.sort_to_folder_by_date()
 
 
+# REMOVER -----------------------------------------------------------------------------------
 
-#############################################################
-###Remover###################################################
-#############################################################
+
 class Remover(object):
     """Contains functions for undoing actions from Sorter class"""
 
     def __init__(self):
         # same as from Sorter class
-        self.MONTHS = ["(1) Jan", "(2) Feb", "(3) Mar", "(4) Apr", "(5) May", "(6) Jun", "(7) Jul", "(8) Aug", "(9) Sep", "(10) Oct", "(11) Nov", "(12) Dec"]
+        self.MONTHS = [
+            "(1) Jan",
+            "(2) Feb",
+            "(3) Mar",
+            "(4) Apr",
+            "(5) May",
+            "(6) Jun",
+            "(7) Jul",
+            "(8) Aug",
+            "(9) Sep",
+            "(10) Oct",
+            "(11) Nov",
+            "(12) Dec",
+        ]
 
     def empty_date_folders(self, sorted_folder, folders):
         """Moves files out of generated date folders so that a different sort type can be chosen"""
@@ -225,15 +294,10 @@ class Remover(object):
             for month in self.MONTHS:
                 folder_path = "".join([sorted_folder, folder, "/", month])
                 for filename in os.listdir(folder_path):
-                    old_path = "".join([folder_path , "/", filename])
+                    old_path = "".join([folder_path, "/", filename])
                     new_path = "".join([sorted_folder, filename])
-                # try block so program doesn't stop if file already exists in directory. Exising file is replaced by file being moved
-                    try:
-                        os.rename(old_path, new_path)
-                    except FileExistsError:
-                        os.remove(new_path)
-                        os.rename(old_path, new_path)
 
+                    shutil.move(old_path, new_path)
 
     def empty_file_type_folders(self, sorted_folder, folders):
         """Moves files out of generated file type folders so that a different sort type can be chosen"""
@@ -243,13 +307,8 @@ class Remover(object):
             for filename in os.listdir(folder_path):
                 old_path = "".join([sorted_folder, folder, "/", filename])
                 new_path = "".join([sorted_folder, filename])
-                #try block so program doesn't stop if file already exists in directory. Exising file is replaced by file being moved
-                try:
-                    os.rename(old_path, new_path)
-                except FileExistsError:
-                    os.remove(new_path)
-                    os.rename(old_path, new_path)
 
+                shutil.move(old_path, new_path)
 
     def delete_date_folders(self, sorted_folder, folders):
         """Deletes folders made by Sorter if sort type == date. Folders must be empty."""
@@ -261,14 +320,17 @@ class Remover(object):
                 try:
                     os.rmdir(path)
                 except:
-                    print(f"Please check {path} exists. Make sure to select options so that same folders as the ones that exist would be created i.e. same sort type and same earliest year if date is chosen.")
+                    print(
+                        f"Please check {path} exists. Make sure to select options so that same folders as the ones that exist would be created i.e. same sort type and same earliest year if date is chosen."
+                    )
 
             path = "".join([sorted_folder, folder])
             try:
                 os.rmdir(path)
             except:
-                print(f"Please check {path} exists. Make sure to select options so that same folders as the ones that exist would be created i.e. same sort type and same earliest year if date is chosen.")
-
+                print(
+                    f"Please check {path} exists. Make sure to select options so that same folders as the ones that exist would be created i.e. same sort type and same earliest year if date is chosen."
+                )
 
     def delete_file_type_folders(self, sorted_folder, folders):
         """Deletes folders made by Sorter if sort type == file type. Folders must be empty."""
@@ -279,8 +341,9 @@ class Remover(object):
             try:
                 os.rmdir(path)
             except:
-                print(f"Please check {path} exists. Make sure to select options so that same folders as the ones that exist would be created i.e. same sort type and same earliest year if date is chosen.")
-
+                print(
+                    f"Please check {path} exists. Make sure to select options so that same folders as the ones that exist would be created i.e. same sort type and same earliest year if date is chosen."
+                )
 
     def undo_folders(self, sorted_folder, sort_type, folders):
         """Executes functions to empty and delete folders created by Sorter class functions"""
